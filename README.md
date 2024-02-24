e070f1d760eee80861e957d2e37f14c00f021d09
Mateo Covacho Berrocal <macovach@c2r17s1.42madrid.com>
2024-02-24 19:12:51 +0100

    asdasd

--- README.md
+++ README.md
@@ -13,25 +13,20 @@
 - [ ] **Objective**: Create a program `rush-02` that converts numbers into their written English value.
 - [ ] **Compilation**: Use `make fclean` and `make`.
 - [ ] **Input**:
-   - [ ] If there's one argument, it's the number to convert.
-   - [ ] If there are two arguments, the first is a custom reference dictionary and the second is the number to convert.
+- [ ]
+  - [ ] If there's one argument, it's the number to convert.
+  - [ ] If there are two arguments, the first is a custom reference dictionary and the second is the number to convert.
 - [ ] **Validations**:
-   - [ ] Return `Error` with a newline if the argument is not an unsigned integer.
-   - [ ] Handle numbers beyond the range of `unsigned int`.
+  - [ ] Return `Error` with a newline if the argument is not an unsigned integer.
+  - [ ] Handle numbers beyond the range of `unsigned int`.
 - [ ] **Language**: The program should operate in English.
 - [ ] **Dictionary**:
-   - [ ] Analyze and use for printing results.
-   - [ ] Modifying values is allowed, but initial keys cannot be removed.
-   - [ ] Format: `[number][spaces]:[spaces][text]`.
-   - [ ] "Dict Error\n" if there are errors in the analysis.
+  - [ ] Analyze and use for printing results.
+  - [ ] Modifying values is allowed, but initial keys cannot be removed.
+  - [ ] Format: `[number][spaces]:[spaces][text]`.
+  - [ ] "Dict Error\n" if there are errors in the analysis.
 - [ ] **Memory Management**: Properly free any allocated memory.
-- [ ] **Usage Examples**:
-   - [ ] `./rush-02 42` → "forty two"
-   - [ ] `./rush-02 0` → "zero"
-   - [ ] `./rush-02 10.4` → "Error"
-   - [ ] `./rush-02 100000` → "one hundred thousand"
-   - [ ] `./rush-02 20` (with `20 : hey everybody !` in the dictionary) → "hey everybody !"
-
+dictionary) → "hey everybody !"
 
 Hola mateo
 
--- ex00/main.c
+++ ex00/main.c
@@ -1,4 +1,19 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+void check_argument_validity(char *str,int *error_flag)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			*error_flag = 1;
+		i++;
+	}
+}
+
 int	ft_atoi(const char *str)
 {
 	int	neg;
@@ -10,10 +25,7 @@ int	ft_atoi(const char *str)
 	number = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
 		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-	
-	{
 		i++;
-	}
 	
 	while (str[i] == '-' || str[i] == '+')
 	{
@@ -30,47 +42,77 @@ int	ft_atoi(const char *str)
 	return (number * neg);
 }
 
+int count_digits( int value )
+{
+    int result = 0;
+    while( value != 0 ) {
+       value /= 10;
+       result++;
+    }
+    return result;
+}
+
+void fill_num_array(int *arr, int n)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i]) 
+		i++;
+	while(n != 0)
+	{
+		arr[i] = 2;
+		n = n / 10;
+		i--;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-    int amount_digits;
     int n;
-	char *arg_str_nums;
-	int pos_nums_arg;
+	char *str_arg_nums;
+	int error;
+	int *numbers_array;
+	int i;
 
-
+	error = 0;
 	// Pasar los argumentos a un array de ints
-    
-	if (argc == 2) // custom dict
+
+	if (argc == 2)
+		str_arg_nums = argv[1];
+	else if (argc == 3)
+		str_arg_nums = argv[2];
+
+
+	n = ft_atoi(str_arg_nums);
+
+	check_argument_validity(str_arg_nums, &error);
+
+
+	printf("error: %i\n ", error);
+	if (error)
 	{
-		arg_str_nums = argv[2];
-
-		n = ft_atoi(arg_str_nums);
-
-	}	
-	else if (argc == 1) // base case
-	{
-		arg_str_nums = argv[1];
-
-		// nums string to ints
-
-
-		// ft_atoi with negative symobls later if n < 0 error
-
-		n = ft_atoi(arg_str_nums);
-		
-
-
+		write(1,"Error\n",6);
+		return (1);
 	}
-	// else
-	// {
-	// 	// TODO: error de argumentos invalidos 
-	// }
+	printf("\natoi results: %i\n", n);
 
+	// chapter 2: mapear a array
 
+	printf("digits: %i\n", count_digits(n));
 
-
+	numbers_array = malloc(sizeof(int) * count_digits(n) + 1);
+	numbers_array[count_digits(n) + 1] = '\0';
+	i = count_digits(n) - 1;
+	while (i >= 0)
+	{	
+		numbers_array[i] = n % 10;
+		n = n / 10;
+		i--;
+	}
+	printf("nums array pos 0: %i\n", numbers_array[2]);
 
 
     return (0);
 }
+
